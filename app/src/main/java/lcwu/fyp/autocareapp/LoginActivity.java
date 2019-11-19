1a35fa4498253b491664a71be5fe7413e977d859
shaheera <afsheenshaheera123@gmail.com>
2019-11-19 14:49:23 +0500

    login.xml designed

--- app/src/main/java/lcwu/fyp/autocareapp/LoginActivity.java
+++ app/src/main/java/lcwu/fyp/autocareapp/LoginActivity.java
@@ -2,17 +2,24 @@ package lcwu.fyp.autocareapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+     TextView gotologin;
+     EditText edtPhoneNo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
 
         /*
         This activity is created and opened when SplashScreen finishes its animations.
@@ -25,5 +32,43 @@ public class LoginActivity extends AppCompatActivity {
         View relativeLayout=findViewById(R.id.login_container);
         Animation animation= AnimationUtils.loadAnimation(this,android.R.anim.fade_in);
         relativeLayout.startAnimation(animation);
+
+
+
+          edtPhoneNo=findViewById(R.id.edtPhoneNo);
+          gotologin=findViewById(R.id.gotologin);
+
+
+          gotologin.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id =v.getId();
+        switch (id)
+        {
+            case R.id.btnRegister:{
+                String strPhoneNo=edtPhoneNo.getText().toString();
+
+                if(strPhoneNo.length()!=11)
+                {
+                    edtPhoneNo.setError("INVALID");
+                }
+                else
+                    {
+                    edtPhoneNo.setError(null);
+                    }
+                break;
+
+            }
+            case R.id.gotologin:
+                {
+                Intent it = new Intent(LoginActivity.this, RegistrationActivity.class);
+                startActivity(it);
+                break;
+            }
+        }
     }
 }
