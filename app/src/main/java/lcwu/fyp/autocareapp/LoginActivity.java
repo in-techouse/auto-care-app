bae58dfd5e476a9b92689188667994ae9bc6d634
Muhammad Taimoor Sultani <team.dev.man@gmail.com>
2019-11-20 16:09:13 +0500

    Working on Phone OTP

--- app/src/main/java/lcwu/fyp/autocareapp/LoginActivity.java
+++ app/src/main/java/lcwu/fyp/autocareapp/LoginActivity.java
@@ -1,8 +1,10 @@
 package lcwu.fyp.autocareapp;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,6 +13,7 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.google.firebase.FirebaseException;
@@ -24,6 +27,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
      TextView goToRegistration;
      EditText edtPhoneNo;
      Button btnLogin;
+     ProgressBar loginProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,10 +48,10 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         relativeLayout.startAnimation(animation);
 
 
-
         edtPhoneNo = findViewById(R.id.edtPhoneNo);
         goToRegistration = findViewById(R.id.goToRegistration);
         btnLogin = findViewById(R.id.btnLogin);
+        loginProgress = findViewById(R.id.loginProgress);
 
 
         goToRegistration.setOnClickListener(this);
@@ -59,35 +63,31 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         int id =v.getId();
         switch (id){
             case R.id.btnLogin:{
-                String strPhoneNo = edtPhoneNo.getText().toString();
+                final String strPhoneNo = edtPhoneNo.getText().toString();
                 if(strPhoneNo.length()!= 13){
-                    edtPhoneNo.setError("INVALID");
+                    edtPhoneNo.setError("Enter valid phone number");
                 }
                 else{
-                    Log.e("LOGIN", "Else part");
-                    edtPhoneNo.setError(null);
-                    FirebaseAuth auth = FirebaseAuth.getInstance();
-                    PhoneAuthProvider provider = PhoneAuthProvider.getInstance();
-                    PhoneAuthProvider.OnVerificationStateChangedCallbacks callBack;
-                    callBack = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
-                        @Override
-                        public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
-                            super.onCodeSent(s, forceResendingToken);
-                            Log.e("LOGIN", "Code Sent, Verification Id: " + s);
-                        }
 
-                        @Override
-                        public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
-                            Log.e("LOGIN", "Verification Completed");
-                        }
 
-                        @Override
-                        public void onVerificationFailed(@NonNull FirebaseException e) {
-                            Log.e("LOGIN", "Verification Failed: " + e.getMessage());
-                        }
-                    };
-                    provider.verifyPhoneNumber(strPhoneNo, 120, TimeUnit.SECONDS, this, callBack);
-                    Log.e("LOGIN", "Code Sent");
+                    AlertDialog dialog = new AlertDialog.Builder(LoginActivity.this)
+                            .setTitle(strPhoneNo)
+                            .setMessage("Is the phone number correct?")
+                            .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                    verifyUser(strPhoneNo);
+                                }
+                            }).setNegativeButton("No, edit number", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            }).show();
+
+
+
                 }
                 break;
 
@@ -98,5 +98,36 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 break;
             }
         }
+    }
+
+    private void verifyUser(String strPhoneNo){
+        Log.e("LOGIN", "Else part");
+        loginProgress.setVisibility(View.VISIBLE);
+        btnLogin.setVisibility(View.GONE);
+        edtPhoneNo.setError(null);
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        PhoneAuthProvider provider = PhoneAuthProvider.getInstance();
+        PhoneAuthProvider.OnVerificationStateChangedCallbacks callBack;
+        callBack = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            @Override
+            public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                super.onCodeSent(s, forceResendingToken);
+                loginProgress.setVisibility(View.GONE);
+                btnLogin.setVisibility(View.VISIBLE);
+                Log.e("LOGIN", "Code Sent, Verification Id: " + s);
+            }
+
+            @Override
+            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                Log.e("LOGIN", "Verification Completed");
+            }
+
+            @Override
+            public void onVerificationFailed(@NonNull FirebaseException e) {
+                Log.e("LOGIN", "Verification Failed: " + e.getMessage());
+            }
+        };
+        provider.verifyPhoneNumber(strPhoneNo, 120, TimeUnit.SECONDS, this, callBack);
+        Log.e("LOGIN", "Code Sent");
     }
 }
--- app/src/main/java/lcwu/fyp/autocareapp/SplashActivity.java
+++ app/src/main/java/lcwu/fyp/autocareapp/SplashActivity.java
@@ -1,7 +1,5 @@
 package lcwu.fyp.autocareapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
