374ed685c1f502d4f2a4617abcf4fcf6e98a0562
Muhammad Taimoor Sultani <team.dev.man@gmail.com>
2019-11-21 14:21:13 +0500

    OTP verification activity added

---
+++ app/src/main/java/lcwu/fyp/autocareapp/OTPVerification.java
@@ -0,0 +1,14 @@
+package lcwu.fyp.autocareapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OTPVerification extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otpverification);
+    }
+}
