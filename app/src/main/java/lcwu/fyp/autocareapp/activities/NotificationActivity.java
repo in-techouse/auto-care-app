3661e590ff10fa988f15dfc1a743e63534d616e1
annie <sumanajmal345@gmail.com>
2019-12-19 15:35:55 +0500

    New Activity added

---
+++ app/src/main/java/lcwu/fyp/autocareapp/activities/NotificationActivity.java
@@ -0,0 +1,16 @@
+package lcwu.fyp.autocareapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import lcwu.fyp.autocareapp.R;
+
+public class NotificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification);
+    }
+}
