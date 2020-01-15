63974af536aa691449448c0754011529d67f16a7
AKI Bajwa <usmanbajwa0006@gmail.com>
2020-01-15 15:56:19 +0500

    Working on Notifications and Bookings

--- app/src/main/java/lcwu/fyp/autocareapp/activities/BookingActivity.java
+++ app/src/main/java/lcwu/fyp/autocareapp/activities/BookingActivity.java
@@ -1,16 +1,84 @@
 package lcwu.fyp.autocareapp.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.security.PrivateKey;
+import java.util.ArrayList;
+import java.util.List;
+
 import lcwu.fyp.autocareapp.R;
+import lcwu.fyp.autocareapp.director.Session;
+import lcwu.fyp.autocareapp.model.Booking;
+import lcwu.fyp.autocareapp.model.User;
 
 public class BookingActivity extends AppCompatActivity {
+
+    private LinearLayout loading;
+    private TextView noBooking;
+    private RecyclerView bookings;
+    private Session session;
+    private User user;
+    private List <Booking> Data;
+    private DatabaseReference reference= FirebaseDatabase.getInstance().getReference().child("Bookings");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booking);
+
+        loading = findViewById(R.id.loading);
+        noBooking = findViewById(R.id.noBooking);
+        bookings = findViewById(R.id.bookings);
+        session = new Session(BookingActivity.this);
+        user = session.getUser();
+        Data = new ArrayList<>();
+        loadBookings();
+    }
+
+    private void loadBookings(){
+        loading.setVisibility(View.VISIBLE);
+        noBooking.setVisibility(View.GONE);
+        bookings.setVisibility(View.GONE);
+        reference.orderByChild("userId").equalTo(user.getPhone()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                for(DataSnapshot d: dataSnapshot.getChildren()){
+                    Booking b = d.getValue(Booking.class);
+                    if(b!=null){
+                        Data.add(b);
+                    }
+                }
+
+                loading.setVisibility(View.GONE);
+                noBooking.setVisibility(View.GONE);
+                bookings.setVisibility(View.VISIBLE);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                loading.setVisibility(View.GONE);
+                noBooking.setVisibility(View.VISIBLE);
+                bookings.setVisibility(View.GONE);
+
+            }
+        });
     }
 
     @Override
--- app/src/main/java/lcwu/fyp/autocareapp/activities/NotificationActivity.java
+++ app/src/main/java/lcwu/fyp/autocareapp/activities/NotificationActivity.java
@@ -1,16 +1,85 @@
 package lcwu.fyp.autocareapp.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import lcwu.fyp.autocareapp.R;
+import lcwu.fyp.autocareapp.director.Session;
+import lcwu.fyp.autocareapp.model.Booking;
+import lcwu.fyp.autocareapp.model.Notification;
+import lcwu.fyp.autocareapp.model.User;
 
 public class NotificationActivity extends AppCompatActivity {
+
+    private LinearLayout loading;
+    private TextView noRecord;
+    private RecyclerView notifications;
+    private Session session;
+    private User user;
+    private List <Notification> Data;
+    private DatabaseReference reference= FirebaseDatabase.getInstance().getReference().child("Notifications");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
+
+        loading = findViewById(R.id.loading);
+        noRecord = findViewById(R.id.noRecord);
+        notifications = findViewById(R.id.notifiations);
+        session = new Session(NotificationActivity.this);
+        user = session.getUser();
+        Data = new ArrayList<>();
+        loadNotification();
+    }
+
+    private void loadNotification(){
+        loading.setVisibility(View.VISIBLE);
+        noRecord.setVisibility(View.GONE);
+        notifications.setVisibility(View.GONE);
+        reference.orderByChild("userId").equalTo(user.getPhone()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                for(DataSnapshot d: dataSnapshot.getChildren()){
+                    Notification n = d.getValue(Notification.class);
+                    if(n!=null){
+                        Data.add(n);
+                    }
+                }
+
+                loading.setVisibility(View.GONE);
+                noRecord.setVisibility(View.GONE);
+                notifications.setVisibility(View.VISIBLE);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                loading.setVisibility(View.GONE);
+                noRecord.setVisibility(View.VISIBLE);
+                notifications.setVisibility(View.GONE);
+
+            }
+        });
+
     }
 
     @Override
