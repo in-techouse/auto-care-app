ba64824a11c43b8f7341dedbccfd70a1c808388d
AKI Bajwa <usmanbajwa0006@gmail.com>
2020-01-30 15:38:27 +0500

    Working on User Profile

--- app/src/main/java/lcwu/fyp/autocareapp/activities/EditUserProfile.java
+++ app/src/main/java/lcwu/fyp/autocareapp/activities/EditUserProfile.java
@@ -6,21 +6,34 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.FirebaseDatabase;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
+import androidx.core.app.Person;
+
+import android.provider.ContactsContract;
+import android.util.Patterns;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+
 import lcwu.fyp.autocareapp.R;
+import lcwu.fyp.autocareapp.director.Constants;
 import lcwu.fyp.autocareapp.director.Helpers;
 import lcwu.fyp.autocareapp.director.Session;
 import lcwu.fyp.autocareapp.model.User;
 
-public class EditUserProfile extends AppCompatActivity {
+public class EditUserProfile extends AppCompatActivity implements View.OnClickListener {
 
     private ImageView img;
     private Helpers helpers;
@@ -28,6 +41,12 @@ public class EditUserProfile extends AppCompatActivity {
     private User user;
     private String imagePath;
     private boolean isImage;
+    private EditText edtPhoneNo, edtFirstName, edtLastName, edtEmail;
+    private String strPhoneNo, strFirstName, strLastName, strEmail;
+    private Button userProfile;
+    private ProgressBar userProfileProgress;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +67,21 @@ public class EditUserProfile extends AppCompatActivity {
             Glide.with(EditUserProfile.this).load(user.getImage()).into(img);
         }
 
+        edtPhoneNo = findViewById(R.id.edtPhoneNo);
+        edtFirstName = findViewById(R.id.edtFirstName);
+        edtLastName = findViewById(R.id.edtLastName);
+        edtEmail = findViewById(R.id.edtEmail);
+        userProfileProgress = findViewById(R.id.userProfileProgress);
+        userProfile = findViewById(R.id.userProfile);
+        userProfileProgress.setVisibility(View.GONE);
+        userProfile.setOnClickListener(this);
+
+
+        edtPhoneNo.setText(user.getPhone());
+        edtFirstName.setText(user.getFirstName());
+        edtLastName.setText(user.getLastName());
+        edtEmail.setText(user.getEmail());
+
         final FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -65,7 +99,7 @@ public class EditUserProfile extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if(requestCode == 10){
-            if (resultCode == RESULT_OK){
+            if (resultCode == RESULT_OK) {
                 if(data == null){
                     return;
                 }
@@ -119,5 +153,76 @@ public class EditUserProfile extends AppCompatActivity {
             }
         }
         return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id){
+            case R.id.userProfile:{
+                if(!helpers.isConnected(EditUserProfile.this)){
+                    helpers.showNoInternetError(EditUserProfile.this);
+                    return;
+                }
+                boolean flag = isValid();
+                if(flag){
+                    userProfileProgress.setVisibility(View.VISIBLE);
+                    userProfile.setVisibility(View.GONE);
+                    user.setFirstName(strFirstName);
+                    user.setLastName(strLastName);
+                    user.setEmail(strEmail);
+                    final Session session = new Session(EditUserProfile.this);
+                    FirebaseDatabase db = FirebaseDatabase.getInstance();
+                    db.getReference().child("Users").child(strPhoneNo).setValue(user)
+                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    userProfileProgress.setVisibility(View.GONE);
+                                    userProfile.setVisibility(View.VISIBLE);
+                                    session.setSession(user);
+                                    finish();
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            userProfileProgress.setVisibility(View.GONE);
+                            userProfile.setVisibility(View.VISIBLE);
+                            helpers.showError(EditUserProfile.this, Constants.ERROR_SOMETHING_WENT_WRONG);
+                        }
+                    });
+                }
+
+
+            }
+        }
+    }
+
+    private boolean isValid() {
+        boolean flag = true;
+        strFirstName = edtFirstName.getText().toString();
+        strLastName = edtLastName.getText().toString();
+        strEmail = edtEmail.getText().toString();
+        if(strFirstName.length() < 3){
+            edtFirstName.setError(Constants.ERROR_FIRST_NAME);
+            flag = false;
+        }
+        else{
+            edtFirstName.setError(null);
+        }
+        if(strLastName.length() < 3){
+            edtLastName.setError(Constants.ERROR_LAST_NAME);
+            flag = false;
+        }
+        else{
+            edtLastName.setError(null);
+        }
+        if (strEmail.length() < 7 || !Patterns.EMAIL_ADDRESS.matcher(strEmail).matches()){
+            edtEmail.setError(Constants.ERROR_EMAIL);
+            flag = false;
+        }
+        else{
+            edtEmail.setError(null);
+        }
+        return flag;
     }
 }
